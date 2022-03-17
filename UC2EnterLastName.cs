a719a60ad6bec1fb8a3a42036850625344010c64
aswinthottilpalam <aswinp264@gmail.com>
2022-03-17 11:28:57 +0530

    Enter valid Last name

---
+++ UC2EnterLastName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace REGEX_UserRegistration
+{
+    public class UC2EnterLastName
+    {
+        public string Regex_Pattern = "^[A-Z a-z]{3,}$";
+
+        public bool validateLastName(string LastName)
+        {
+            return Regex.IsMatch(LastName, Regex_Pattern);
+        }
+    }
+}
